02b5a85a68bffeadf64771375bef240185436dff
Gyuris Gellért <bubu@ujevangelizacio.hu>
2017-03-23 23:58:47 +0100

    Improve XML converting, files
    remove pure CSS tweaks:
    - tweak="nested"
    - tweak="linebreak"

--- convert_xml_stylesheet.sh
+++ convert_xml_stylesheet.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Replace xml-stylesheet from CSS to XSL and save an another file
+# Replace xml-stylesheet from CSS to XSL, remove tweaks and save an another file
 for file in *.xml
 do
   if [[ "$file" =~ \.xsl\.xml$ ]] ; then
     echo "Skipping: $file"
   else
     echo "-> $file"
-    sed -s 's/href="stylesheets\/openlyrics\.css" type="text\/css"/href="stylesheets\/openlyrics\.xsl" type="text\/xsl"/' "$file" &>"$(basename "$file" .xml).xsl.xml"
+    sed -e 's/href="stylesheets\/openlyrics\.css" type="text\/css"/href="stylesheets\/openlyrics\.xsl" type="text\/xsl"/' \
+        -e 's/ tweak="nested"//' \
+        -e 's/ tweak="linebreak"//' \
+        "$file" &>"$(basename "$file" .xml).xsl.xml"
   fi
 done

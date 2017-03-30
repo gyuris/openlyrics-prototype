e39ef1144b5f80de4bfec721465aea2c4194b711
Gyuris Gellért <bubu@ujevangelizacio.hu>
2017-03-30 23:39:06 +0200

    Melt scripts
    Provide only one process script

---
+++ generate.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# OpenLyrics prototype: generate PDF
+
+# Generate book
+################
+echo "BOOK XML mod: book.xml"
+# Book needs extra xslt tranlsation to do xi:includes but without --xinclude param
+xsltproc book.xml &> book.xsl.xml
+# Replace CSS with XSL and remove CSS tweaks
+sed -i -e 's/href="stylesheets\/openlyrics\.css" type="text\/css"/href="stylesheets\/openlyrics\.xsl" type="text\/xsl"/g' \
+    -e 's/ tweak="nested"//g' \
+    -e 's/ tweak="linebreak"//g' \
+    book.xsl.xml
+
+# Generate XML with another xml-stylesheet
+###########################################
+for file in *.xml
+do
+  if [[ $file == 'book.xml' ]] || [[ "$file" =~ \.xsl\.xml$ ]] ; then
+    # Nothing to do
+    echo "Skip      : $file"
+  else
+    # Modify XML source
+    echo "XML mod   : $file"
+    # Replace CSS with XSL and remove CSS tweaks, save it in another file
+    sed -e 's/href="stylesheets\/openlyrics\.css" type="text\/css"/href="stylesheets\/openlyrics\.xsl" type="text\/xsl"/g' \
+        -e 's/ tweak="nested"//g' \
+        -e 's/ tweak="linebreak"//g' \
+        "$file" &>"$(basename "$file" .xml).xsl.xml"
+  fi
+done
+
+# Generate PDF-s
+#################
+for file in *.xml
+do
+  if [[ "$file" =~ \.xsl\.xml$ ]] ; then
+    # XSLT version: XML->HTML->PDF
+    echo " XML->HTML: $file"
+    # Generate HTML
+    xsltproc "$file"  &> "$(basename "$file" .xml).html"
+    echo "HTML->PDF : $file"
+    # Generate PDF
+    prince "$(basename "$file" .xml).html" -o "pdf/$(basename "$file" .xml).pdf" --pdf-author="Gyuris Gellért"
+    # Remove HTML
+    rm "$(basename "$file" .xml).html"
+  else
+    # CSS version: XML->PDF
+    echo " XML->PDF : $file"
+    # Generate PDF
+    prince "$file" -o "pdf/$(basename "$file" .xml).pdf" --pdf-author="Gyuris Gellért"
+  fi
+done
